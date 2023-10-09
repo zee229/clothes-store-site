91216b2f60ee9eef3ad1f3e9588f8b5b9c1edaa8
Nikita Yastreb <yastrebnikita723@gmail.com>
2023-10-10 00:05:56 +0300

    new url with product category and brand filters

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,9 @@ app_name = 'products'
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='index'),
-    path('category/<int:category_id>', ProductsListView.as_view(), name='category'),
+    # path('category/<int:category_id>', ProductsListView.as_view(), name='category'),
+    # path('brand/<int:brand_id>', ProductsListView.as_view(), name='brand'),
+    path('filters/category=<int:category_id>&brand=<int:brand_id>', ProductsListView.as_view(), name='filters'),
     path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
