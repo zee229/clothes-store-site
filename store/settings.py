1737c8b35dcfb195d1ff36a081f690cec3f6ad0e
yastrebnikita723@gmail.com <yastrebnikita723@gmail.com>
2023-10-10 12:09:16 +0300

    +STATIC_ROOT

--- store/settings.py
+++ store/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from pathlib import Path
 
 import environ
@@ -184,6 +185,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
